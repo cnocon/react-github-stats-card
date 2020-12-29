ed26f59e07e3ad5b6289954760afbb342086dd69
Cristin O'Connor <her@cristin.io>
2020-12-29 10:22:38 -0700

    Layout updates and theme polishing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from './components/Card.js';
 import './App.css';
 
 function App() {
-  return (<Card username='cnocon' theme={true} accessToken='bb9fbbf8145c7272dc7941d8a530cc09f9a1e7e1' />)
+  return <Card username='cnocon' theme={true} accessToken="bb9fbbf8145c7272dc7941d8a530cc09f9a1e7e1"/>
 }
 
 export default App;
